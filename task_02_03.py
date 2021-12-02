bc5375dbcad4f7531b92236aab7bd51b0cfc384e
milena173 <497315@gmail.com>
2021-12-02 18:26:44 +0400

    task_02_03

---
+++ task_02_03.py
@@ -0,0 +1,25 @@
+seasons = {'Выбранный месяц зимний': (1, 2, 12),
+           'Выбранный месяц весенний': (3, 4, 5),
+           'Выбранный месяц летний': (6, 7, 8),
+           'Выбранный месяц осенний': (9, 10, 11)}
+
+month = int(input("Введите порядковый номер месяца (напр. 2)"))
+for key in seasons.keys():
+    if month in seasons[key]:
+        print(key)
+
+
+# вариант 2
+winter = [1, 2, 12]
+spring = [3, 4, 5]
+summer = [6, 7, 8]
+autumn = [9, 10, 11]
+month = int(input("Введите порядковый номер месяца (напр. 2)"))
+if month in winter:
+    print(f'Выбранный месяц зимний')
+elif month in spring:
+    print(f'Выбранный месяц весенний')
+elif month in summer:
+    print(f'Выбранный месяц летний')
+elif month in autumn:
+    print(f'Выбранный месяц осенний')
